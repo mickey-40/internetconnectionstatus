96270af0c684610d88882c2d3d0415ed96d57b0e
mickey-40 <mickeyarnold53@yahoo.com>
2023-12-24 17:13:37 -0600

    finished script.js

--- script.js
+++ script.js
@@ -3,10 +3,38 @@ const image = document.getElementById("image");
 const statusDisplay = document.getElementById("status");
 const bgColor = document.getElementById("main");
 
+function setColor () {
+  bgColor.classList.add("online")
+}
 
 async function connectionStatus (){
   try {
     const fetchResult = await fetch('https://upload.wikimedia.org/wikipedia/en/thumb/7/7d/Lenna_%28test_image%29.png/440px-Lenna_%28test_image%29.png?time='+ (new Date()).getTime());
+    image.src = "./images/online.png";
+    setColor();
+    return fetchResult.status >= 200 && fetchResult.status < 300;
+  } catch (error) {
+    // console.log(error);
+    statusDisplay.textContent = "Oops!! Your internet connection is down.";
+    image.src = "./images/offline.png";
+    bgColor.classList.remove("online");
   }
 }
 
+//Monitor the connection
+setInterval(async () => {
+  const result = await connectionStatus();
+  if(result) {
+    statusDisplay.textContent = "You are online, connection looks good.";
+    setColor();
+  }
+}, 5000);
+
+//Check connection when the page loads.
+window.addEventListener("load", async (e) => {
+  if(connectionStatus()) {
+    statusDisplay.textContent = "You are online.";
+  } else {
+    statusDisplay.textContent = "You are offline.";
+  }
+})
